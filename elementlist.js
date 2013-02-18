74d7e11deeb9b5e29db6f8663af230bcf36935f1
Florian Wolters <wolters.fl@gmail.com>
2013-02-18 15:20:59 +0100

    Updated to latest version.

--- elementlist.js
+++ elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","FlorianWolters\\Component\\Util\\Singleton\\MultitonTrait"],["c","FlorianWolters\\Component\\Util\\Singleton\\ReflectionTrait"],["c","FlorianWolters\\Component\\Util\\Singleton\\SingletonTrait"]];
+ApiGen.elements = [["c","FlorianWolters\\Component\\Util\\Singleton\\MultitonTrait"],["c","FlorianWolters\\Component\\Util\\Singleton\\SingletonTrait"]];
