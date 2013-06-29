3846447dae28e812a45dad8989429e00a16dea1b
Florian Wolters <wolters.fl@gmail.com>
2013-06-29 14:34:34 +0200

    Updated @copyright.

--- src/tests/mocks/php/FlorianWolters/Mock/SingletonWithArguments.php
+++ src/tests/mocks/php/FlorianWolters/Mock/SingletonWithArguments.php
@@ -3,7 +3,7 @@ namespace FlorianWolters\Mock;
 
 /**
  * @author    Florian Wolters <wolters.fl@gmail.com>
- * @copyright 2012 Florian Wolters
+ * @copyright 2012-2013 Florian Wolters
  * @license   http://gnu.org/licenses/lgpl.txt LGPL-3.0+
  * @link      http://github.com/FlorianWolters/PHP-Component-Util-Singleton
  * @since     Class available since Release 0.3.0
--- src/tests/mocks/php/FlorianWolters/Mock/SingletonWithProtectedCreationMethod.php
+++ src/tests/mocks/php/FlorianWolters/Mock/SingletonWithProtectedCreationMethod.php
@@ -5,7 +5,7 @@ use FlorianWolters\Component\Util\Singleton\SingletonTrait;
 
 /**
  * @author    Florian Wolters <wolters.fl@gmail.com>
- * @copyright 2012 Florian Wolters
+ * @copyright 2012-2013 Florian Wolters
  * @license   http://gnu.org/licenses/lgpl.txt LGPL-3.0+
  * @link      http://github.com/FlorianWolters/PHP-Component-Util-Singleton
  * @since     Class available since Release 0.3.0
