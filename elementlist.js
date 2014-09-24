15dc5073dc0e76661eae32b8c3b773a06f0b3d21
Florian Wolters <wolters.fl@gmail.com>
2014-09-24 19:33:09 +0200

    Updated API documentation with "Sami".
    Signed-off-by: Florian Wolters <wolters.fl@gmail.com>

--- elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","FlorianWolters\\Component\\Util\\Singleton\\MultitonTrait"],["c","FlorianWolters\\Component\\Util\\Singleton\\SingletonTrait"]];
---
+++ tree.js
@@ -0,0 +1,8 @@
+var tree = [["FlorianWolters",,"",
+                [["Component",,"",
+                [["Util",,"",
+                [["Singleton","FlorianWolters\/Component\/Util\/Singleton.html","",
+                [["MultitonTrait","FlorianWolters\/Component\/Util\/Singleton\/MultitonTrait.html","",
+                [        ]],["SingletonTrait","FlorianWolters\/Component\/Util\/Singleton\/SingletonTrait.html","",
+                [        ]]        ]]        ]]        ]]        ]]        ]
+
